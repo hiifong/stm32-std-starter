3cdbc61e7e513ff2cf9e79c2c78152433745ff6e
hiifong <i@hiif.ong>
2025-07-05 01:18:33 +0800

    EXTI 2

--- User/Src/main.c
+++ User/Src/main.c
@@ -5,56 +5,79 @@
 #include "stm32f10x.h"
 #include "OLED.h"
 
-uint16_t count = 0;
+static int16_t tmp;
+int16_t num;
+int16_t count;
 
-void CountCensor_Init() {
+void Encoder_Init() {
     // GPIO
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
     GPIO_InitTypeDef GPIO_InitStructure;
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_14;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_Init(GPIOB, &GPIO_InitStructure);
 
     // AFIO
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
-    GPIO_EXTILineConfig(GPIO_PortSourceGPIOB, GPIO_PinSource14);
+    GPIO_EXTILineConfig(GPIO_PortSourceGPIOB, GPIO_PinSource0);
+    GPIO_EXTILineConfig(GPIO_PortSourceGPIOB, GPIO_PinSource1);
 
     // EXTI
     EXTI_InitTypeDef EXTI_InitStructure;
-    EXTI_InitStructure.EXTI_Line = EXTI_Line14;
+    EXTI_InitStructure.EXTI_Line = EXTI_Line0 | EXTI_Line1;
     EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
-    EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Rising;
+    EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Falling;
     EXTI_InitStructure.EXTI_LineCmd = ENABLE;
     EXTI_Init(&EXTI_InitStructure);
 
     // NVIC
     NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
     NVIC_InitTypeDef NVIC_InitStructure;
-    NVIC_InitStructure.NVIC_IRQChannel = EXTI15_10_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannel = EXTI0_IRQn;
     NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
     NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
     NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
     NVIC_Init(&NVIC_InitStructure);
+
+    NVIC_InitStructure.NVIC_IRQChannel = EXTI1_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 2;
+    NVIC_Init(&NVIC_InitStructure);
 }
 
-uint16_t CountSensor_GetCount() {
-    return count;
+int16_t Encoder_GetCount() {
+    tmp = count;
+    count = 0;
+    return tmp;
 }
 
 void Main() {
-    CountCensor_Init();
+    Encoder_Init();
     OLED_Init();
-    OLED_ShowString(1,1, "Count:");
+    OLED_ShowString(1,1, "Num:");
     while (1)
     {
-        OLED_ShowNum(1,7, CountSensor_GetCount(), 5);
+        num += Encoder_GetCount();
+        OLED_ShowNum(1,5, num, 5);
     }
 }
 
-void EXTI15_10_IRQHandler(void) {
-    if (EXTI_GetITStatus(EXTI_Line14) == SET) {
-        count++;
-        EXTI_ClearITPendingBit(EXTI_Line14);
+void EXTI0_IRQHandler(void) {
+    if (EXTI_GetITStatus(EXTI_Line0) == SET) {
+        if (GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_1) == 0) {
+            count--;
+        }
+        EXTI_ClearITPendingBit(EXTI_Line0);
+    }
+}
+
+void EXTI1_IRQHandler(void) {
+    if (EXTI_GetITStatus(EXTI_Line1) == SET) {
+        if (GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_0) == 0) {
+            count++;
+        }
+        EXTI_ClearITPendingBit(EXTI_Line1);
     }
 }
