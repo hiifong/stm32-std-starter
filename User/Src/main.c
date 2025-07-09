1af14b5377d977d65e9d10b44917442cb094a284
hiifong <i@hiif.ong>
2025-07-09 20:59:20 +0800

    PWM/1

--- User/Src/main.c
+++ User/Src/main.c
@@ -2,82 +2,66 @@
 // Created by hiifong on 2025/7/1.
 //
 
+#include "delay.h"
 #include "stm32f10x.h"
 #include "OLED.h"
 
-static int16_t tmp;
-int16_t num;
-int16_t count;
+uint8_t i;
 
-void Encoder_Init() {
+void PWM_Init(void) {
+    // 时钟使能
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+
     // GPIO
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
     GPIO_InitTypeDef GPIO_InitStructure;
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP; // 复用推挽输出
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_Init(GPIOB, &GPIO_InitStructure);
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
 
-    // AFIO
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
-    GPIO_EXTILineConfig(GPIO_PortSourceGPIOB, GPIO_PinSource0);
-    GPIO_EXTILineConfig(GPIO_PortSourceGPIOB, GPIO_PinSource1);
+    // 内部时钟, 可以省略（上电后定时器默认使用内部时钟）
+    TIM_InternalClockConfig(TIM2);
 
-    // EXTI
-    EXTI_InitTypeDef EXTI_InitStructure;
-    EXTI_InitStructure.EXTI_Line = EXTI_Line0 | EXTI_Line1;
-    EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
-    EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Falling;
-    EXTI_InitStructure.EXTI_LineCmd = ENABLE;
-    EXTI_Init(&EXTI_InitStructure);
+    // 时基单元
+    TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+    TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1; // 时钟分频
+    TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up; // 计数模式
+    TIM_TimeBaseStructure.TIM_Period = 100 - 1;       // 周期ARR，在10K的计数频率下记10000个数就是1s
+    TIM_TimeBaseStructure.TIM_Prescaler = 720 - 1;     // 预分频PSC， 这里是对72M进行7200分频，得到10K的计数频率
+    TIM_TimeBaseStructure.TIM_RepetitionCounter = 0;    // 重复计数器，高级定时器才有
+    TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
 
-    // NVIC
-    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
-    NVIC_InitTypeDef NVIC_InitStructure;
-    NVIC_InitStructure.NVIC_IRQChannel = EXTI0_IRQn;
-    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
-    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
-    NVIC_Init(&NVIC_InitStructure);
+    // 输出比较单元
+    TIM_OCInitTypeDef TIM_OCInitStructure;
+    TIM_OCStructInit(&TIM_OCInitStructure); // 初始化OCInitStructure
+    TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1; // PWM模式1
+    TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+    TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+    TIM_OCInitStructure.TIM_Pulse = 0;   // 脉冲，用来设置CCR寄存器值
+    TIM_OC1Init(TIM2,&TIM_OCInitStructure);
 
-    NVIC_InitStructure.NVIC_IRQChannel = EXTI1_IRQn;
-    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
-    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 2;
-    NVIC_Init(&NVIC_InitStructure);
+    // 启动定时器
+    TIM_Cmd(TIM2, ENABLE);
 }
 
-int16_t Encoder_GetCount() {
-    tmp = count;
-    count = 0;
-    return tmp;
+void PWM_SetCompare1(uint16_t compare) {
+    TIM_SetCompare1(TIM2, compare);
 }
 
 void Main() {
-    Encoder_Init();
     OLED_Init();
-    OLED_ShowString(1,1, "Num:");
+    PWM_Init();
+    OLED_ShowString(1,1, "Hello World!");
     while (1)
     {
-        num += Encoder_GetCount();
-        OLED_ShowNum(1,5, num, 5);
-    }
-}
-
-void EXTI0_IRQHandler(void) {
-    if (EXTI_GetITStatus(EXTI_Line0) == SET) {
-        if (GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_1) == 0) {
-            count--;
+        for (i = 0; i <= 100; i++) {
+            PWM_SetCompare1(i);
+            Delay_ms(10);
         }
-        EXTI_ClearITPendingBit(EXTI_Line0);
-    }
-}
-
-void EXTI1_IRQHandler(void) {
-    if (EXTI_GetITStatus(EXTI_Line1) == SET) {
-        if (GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_0) == 0) {
-            count++;
+        for (i = 0; i <= 100; i++) {
+            PWM_SetCompare1(100-i);
+            Delay_ms(10);
         }
-        EXTI_ClearITPendingBit(EXTI_Line1);
     }
 }
